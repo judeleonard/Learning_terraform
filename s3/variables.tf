bf09e39fe7d5552c3834b512a81b2993890226e7
judeleonard <judeleonard86@gmail.com>
2023-01-12 11:35:33 +0000

    initial commit

---
+++ s3/variables.tf
@@ -0,0 +1,20 @@
+variable "aws_access_key" {
+default = "AKIA2FLDO5XMO3NX4RUL"
+}
+
+variable "aws_secret_key" {
+default = "ZQdYLrHdNr/mGv1ksi8zdT5jQEZWTMAn8UXmwn45"
+ }
+
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "bucket_name" {
+    default = "judebucket-1"
+  
+}
+
+variable "acl_value" {
+    default = "private"
+}
